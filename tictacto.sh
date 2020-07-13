6d11da8a9e0bb7f4a870a6d26e414f01c6deee49
mrunal93 <mrn26893@gmail.com>
2020-07-13 08:05:29 +0530

    [Mrunal]Add: Created the tic tac Board and a reset function

--- tictacto.sh
+++ tictacto.sh
@@ -1,4 +1,36 @@
-#!/bin/bash
+#!/bin/bash 
 
-echo "Welcome to Tic-Tac-Toe Game"
+echo -e "Welcome to Tic-Tac-Toe Game \n----------------------------- \nAs a Tic Tac Toe player would like to challenge Computer"
 
+ROW_SIZE=3
+BOARD_SIZE=$((ROW_SIZE*ROW_SIZE))
+Position=0
+count=0
+declare -a ticBoard
+
+resetBoard() {
+	for (( position=1; position<=$BOARD_SIZE; position++ ))
+	do
+		ticBoard[$position]=0
+	done
+}
+
+
+displayBoard() {
+	for (( count=1; count<=$BOARD_SIZE; count++ ))
+	do
+		if [[ ${ticBoard[$count]} -eq 0 ]]
+		then
+			printf _"|"
+		else
+			printf ${ticBoard[$count]}" "
+		fi
+
+		if [ $(( $count % $ROW_SIZE )) -eq 0 ]
+		then
+			echo
+		fi
+	done
+}
+
+displayBoard
