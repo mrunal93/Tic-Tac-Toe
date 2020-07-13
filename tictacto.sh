36ee5766cbb9021945ff24e8cd98803001c7d79e
mrunal93 <mrn26893@gmail.com>
2020-07-13 12:38:38 +0530

    [Mrunal]Refractor: removed the displayBoard function

--- tictacto.sh
+++ tictacto.sh
@@ -1,11 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
 echo -e "Welcome to Tic-Tac-Toe Game \n----------------------------- \nAs a Tic Tac Toe player would like to challenge Computer"
 
 ROW_SIZE=3
 BOARD_SIZE=$((ROW_SIZE*ROW_SIZE))
 Position=0
-count=0
 declare -a ticBoard
 
 resetBoard() {
@@ -15,22 +14,3 @@ resetBoard() {
 	done
 }
 
-
-displayBoard() {
-	for (( count=1; count<=$BOARD_SIZE; count++ ))
-	do
-		if [[ ${ticBoard[$count]} -eq 0 ]]
-		then
-			printf _"|"
-		else
-			printf ${ticBoard[$count]}" "
-		fi
-
-		if [ $(( $count % $ROW_SIZE )) -eq 0 ]
-		then
-			echo
-		fi
-	done
-}
-
-displayBoard
